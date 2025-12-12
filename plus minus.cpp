969b9d4da7afd27dd6082064c2e0c7fa509d7156
ALOK JHA <alokjha1229@gmail.com>
2025-12-12 18:28:07 +0530

    Add files via upload

---
+++ plus minus.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ int main(){
+    int num;
+    cout<<"Enter num :";
+    cin>>num;
+
+    if (num>=0){
+        cout<<"the nummber is positive";
+    
+    } else {
+        cout<<"the number is negative";
+
+    }
+    return 0;
+
+ }
